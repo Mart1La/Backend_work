7a32ce2d53b19e3d9bc0cfc4f4d3a31b183bdbf5
Martin Lambotte <martin.lambotte@etu.minesparis.psl.eu>
2025-04-29 22:48:17 +0200

    vf qui marche en local avec uvicorn

--- pixel_war/main.py
+++ pixel_war/main.py
@@ -1,3 +1,21 @@
+"""
+---------- QUELQUES INFOS POUR CETTE VERSION ---------- 
+Consignes frontend : https://github.com/ue22-p24/web-pixels-war
+Le frontend est constitué du html, css et js. Le Backend du py.
+
+Pour lancer le serveur en local, on a utilisé la commande suivante
+dans bash : uvicorn main:app --reload
+
+Le choix de la map (TEST ou 0000) se fait "à la main" et
+    - si TEST est choisi, le délai entre deux changements est 1s
+    - si 0000 est choisi, le délai entre deux changements est 10s
+
+Les fichiers sont ici configurés pour une utilisation locale,
+on pourra changer la ligne 44 du py, la ligne 2 du js
+    
+"""
+
+# Importation des modules utiles
 from copy import deepcopy
 import time
 from uuid import uuid4
@@ -11,32 +29,24 @@ from fastapi.templating import Jinja2Templates
 # Instance de FastAPI
 app = FastAPI()
 
-# Fichiers statiques
+# Fichiers statiques (css et js)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Templates
 templates = Jinja2Templates(directory="templates")
 
+# Permet d'afficher la page HTML dès le début
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# On peut utiliser dans bash (pour lancer le serveur)
-# uvicorn main:app --reload
-
-# Permet d'autoriser le programme a fonctionner
+# Permet d'autoriser le programme à fonctionner (à changer si plus en local)
 app.add_middleware(CORSMiddleware,
                    allow_origins=["http://localhost:8000"],
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"]
                    )
-
-"""
-Les consignes coté frontend sont les suivantes: 
-https://github.com/ue22-p24/web-pixels-war
-"""
-
 
 # Informations liées à chaque utilisateur
 class UserInfo:
@@ -46,8 +56,9 @@ class UserInfo:
         self.last_seen_map = deepcopy(carte)
         self.last_edited_time_nanos = 0
 
+# Informations sur chaque carte
 class Carte:
-    keys: set[str]
+    keys: set[str]      # Clés associées à la carte
     nx: int             # Largeur de la grille
     ny: int             # Longueur de la grille
     data: list[list[tuple[int, int, int]]]
@@ -56,7 +67,7 @@ class Carte:
         # Le timeout est en nanosecondes, on considère 1s par défaut
         
         # La carte est modifiée par plein d'utilisateurs auxquels
-        # est associé une unique clé
+        # est associé une clé et un identifiant unique
         self.keys = set() 
         self.nx = nx
         self.ny = ny
@@ -73,24 +84,24 @@ class Carte:
         return key
 
     def is_valid_key(self, key: str):
-        # Returns True if key already in, False elsewise
+        # Returns True if key already known, False otherwise
         return key in self.keys
     
     def create_new_user_id(self):
         user_id = str(uuid4())  # Génère un nouvel identifiant unique pour l'utilisateur
         self.user[user_id] = UserInfo(self.data)
         return user_id
-    
         
     def is_valid_user_id(self, user_id:str):
         return user_id in self.user
 
-
+# Dictionnaire avec les cartes connues, ici il y a seulement 0000 et TEST, avec un délai distinct
 cartes : dict[str, Carte] = {
     "0000": Carte(nx=100, ny=100, timeout_nanos=10_000_000_000),
     "TEST": Carte(nx=10, ny=10)
 }
 
+# Preinitialisation
 @app.get("/api/v1/{nom_carte}/preinit")
 async def preinit(nom_carte: str): # Permet de recup le nom de la carte qu'on met dans la requete
     carte = cartes[nom_carte]
@@ -99,9 +110,12 @@ async def preinit(nom_carte: str): # Permet de recup le nom de la carte qu'on me
 
     key = carte.create_new_key()
     res = JSONResponse({"key": key})
+
+    # On stocke des cookies pour bien identifier d'où viennent les requêtes pour éviter la triche
     res.set_cookie("key", key, secure=True, samesite="none", max_age=3600)
     return res
 
+# Initialisation
 @app.get("/api/v1/{nom_carte}/init")
 async def init(nom_carte: str,
                query_key: str = Query(alias="key"),
@@ -130,10 +144,7 @@ async def init(nom_carte: str,
     res.set_cookie("id", user_id, secure=True, samesite="none", max_age=3600)
     return res
 
-
-# # https://pixels-war.oie-lab.net/api/v1/TEST/deltas?id={user_id}
-
-
+# Inspection des différences
 @app.get("/api/v1/{nom_carte}/deltas")
 async def deltas(nom_carte: str,
                  query_user_id: str = Query(alias="id"),
@@ -142,6 +153,7 @@ async def deltas(nom_carte: str,
     
     carte = cartes[nom_carte]
     
+    # Vérifications de non-triche
     if not carte:
         return {"Error": "Carte inconnue (deltas)"}
     if query_user_id != cookie_user_id:
@@ -167,7 +179,7 @@ async def deltas(nom_carte: str,
         "deltas": deltas
     }
 
-
+# Changement d'un pixel
 @app.get("/api/v1/{nom_carte}/set/{user_id}/{y}/{x}/{r}/{g}/{b}")
 async def change_color(nom_carte : str,
                     user_id : str,
@@ -182,7 +194,7 @@ async def change_color(nom_carte : str,
     
     carte = cartes[nom_carte]
 
-    # Vérifications classiques
+    # Vérifications de non-triche
     if not carte:
         return {"Error": "Carte inconnue (deltas)"}
     if not carte.is_valid_key(cookie_key):
@@ -192,11 +204,11 @@ async def change_color(nom_carte : str,
 
     user_info = carte.user[user_id]
     delta_time_ns = time.time_ns() - user_info.last_edited_time_nanos
+
     if delta_time_ns >= carte.timeout_nanos:
         user_info.last_edited_time_nanos = time.time_ns()
-        user_carte = user_info.last_seen_map
-        # user_carte[x][y] = (r, g, b)
         carte.data[x][y] = (r, g, b)
         return JSONResponse(content=0)
     else:
+        print(f"Attendre {round((carte.timeout_nanos-(delta_time_ns))/1_000_000_000, 2)} seconde(s)")
         return JSONResponse(content=f"Attendre {carte.timeout_nanos-(delta_time_ns)} nanosecondes")
--- pixel_war/static/script.js
+++ pixel_war/static/script.js
@@ -1,38 +1,35 @@
-// pour l'instant on ne peut pas y toucher depuis l'interface
-// il faut recharger la page pour changer de carte
+// Le projet a été configuré en local (voir ligne suivante)
 const PIXEL_URL = "http://127.0.0.1:8000"
 
 // ON PEUT CHANGER LA CARTE ICI : 2 choix (TEST ou 0000)
-// const MAP_ID = "0000"
+// Il faudra recharger la page pour changer de carte
 const MAP_ID = "0000"
 
-document.addEventListener("DOMContentLoaded", () => {
+// Autre constante utile :
+const PREFIX = `${PIXEL_URL}/api/v1/${MAP_ID}`
 
-    const PREFIX = `${PIXEL_URL}/api/v1/${MAP_ID}`
+document.addEventListener("DOMContentLoaded", () => {
     let refreshInterval;
 
-    // pour savoir à quel serveur / carte on s'adresse
-    // on les affiche en dur
-    // pour l'instant on ne peut pas y toucher depuis l'interface
-    // il faut recharger la page pour changer de carte
     document.getElementById("baseurl").value = PIXEL_URL
     document.getElementById("mapid").value = MAP_ID
     document.getElementById("baseurl").readOnly = true
     document.getElementById("mapid").readOnly = true
 
+    // Preinitialisation
     fetch(`${PREFIX}/preinit`, {credentials: "include"})
         .then((response) => response.json())
         .then((json) => {
-            console.log(json)
-            //TODO: maintenant que j'ai le json de preinit, je peux initialiser ma grille
+            
+            // console.log(json)
             let cle = json.key
-            console.log(cle)
+            // console.log(cle)
 
-            //TODO: maintenant que j'ai le JSON, afficher la grille, et récupérer l'id
+            // Recuperation de l'ID et affichage de la grille
             fetch(`${PREFIX}/init?key=${cle}`, {credentials: "include"})
                 .then((response) => response.json())
                 .then((json) => {
-                    // console.log(json)
+
                     let id = json.id
                     let data = json.data
                     let nx = json.nx
@@ -44,20 +41,16 @@ document.addEventListener("DOMContentLoaded", () => {
                     grille.style.gridTemplateRows = `repeat(${ny}, 4px)`
                     dessin_grille(grille, data)
                     
-                    // Rafraichissement de la grille toute les 3 secondes
+                    // Rafraichissement de la grille toutes les 3 secondes
                     refreshInterval = setInterval(() => {
                         refresh(id)
                     }, 3000)
 
-                    //TODO: maintenant que j'ai l'id, attacher la fonction refresh(id), à compléter, au clic du bouton refresh
+                    // Bouton refresh (voir fonction plus loin)
                     let elt = document.getElementById("refresh")
                     elt.addEventListener("click", () => {refresh(id)})
 
-
-                    //TODO: attacher au clic de chaque pixel une fonction qui demande au serveur de colorer le pixel sous là forme :
-                    // http://pixels-war.oie-lab.net/api/v1/0000/set/id/x/y/r/g/b
-                    // la fonction getPickedColorInRGB ci-dessous peut aider
-
+                    // Changement de la couleur du pixel
                     grille.addEventListener("click", (event) => {
                         let color_to_add = getPickedColorInRGB()
                         const cible = event.target;
@@ -66,61 +59,38 @@ document.addEventListener("DOMContentLoaded", () => {
                             refresh(id);
                         }
                     });
-
-                    document.getElementById("colorpicker").addEventListener("input", (event) => {
-                        const color = event.target.value;
-                        console.log(`Couleur choisie : ${color}`);
-                    });  
-
                 })
-
-            // cosmétique / commodité / bonus:
-
-            // TODO: pour être efficace, il serait utile d'afficher quelque part
-            // les coordonnées du pixel survolé par la souris
-
-            //TODO: pour les rapides: afficher quelque part combien de temps
-            // il faut attendre avant de pouvoir poster à nouveau
-
-            //TODO: pour les avancés: ça pourrait être utile de pouvoir
-            // choisir la couleur à partir d'un pixel ?
-
         })
-
-    //TODO: pour les élèves avancés, comment transformer les "then" ci-dessus en "async / await" ?
-    //TODO: pour les élèves avancés, faire en sorte qu'on puisse changer de carte
-    //      voir le bouton Connect dans le HTML
-
-    // À compléter puis à attacher au bouton refresh en passant mon id une fois récupéré
-    function refresh(user_id) {
-        fetch(`${PREFIX}/deltas?id=${user_id}`, {credentials: "include"})
-            .then((response) => response.json())
-            .then((json) => {
-                let modif = json.deltas
-                console.log(modif)
-
-                modif.forEach((element) => {
-                    let pixel = document.getElementById(`${element[0]} ${element[1]}`)
-                    pixel.style.backgroundColor = `rgb(${element[2]}, ${element[3]}, ${element[4]})`
-                })
-                
-
-            })
-    }
-
-    // Petite fonction facilitatrice pour récupérer la couleur cliquée en RGB
-    function getPickedColorInRGB() {
-        const colorHexa = document.getElementById("colorpicker").value
-
-        const r = parseInt(colorHexa.substring(1, 3), 16)
-        const g = parseInt(colorHexa.substring(3, 5), 16)
-        const b = parseInt(colorHexa.substring(5, 7), 16)
-
-        return [r, g, b]
-    }
-
 })
 
+// Fonctions utilisées :
+// 1. Permet le rafraichissement de la grille
+function refresh(user_id) {
+    fetch(`${PREFIX}/deltas?id=${user_id}`, {credentials: "include"})
+        .then((response) => response.json())
+        .then((json) => {
+            let modif = json.deltas
+            console.log(modif)
+
+            modif.forEach((element) => {
+                let pixel = document.getElementById(`${element[0]} ${element[1]}`)
+                pixel.style.backgroundColor = `rgb(${element[2]}, ${element[3]}, ${element[4]})`
+            })
+        })
+}
+
+// 2. Permet de récupérer la couleur cliquée en RGB
+function getPickedColorInRGB() {
+    const colorHexa = document.getElementById("colorpicker").value
+
+    const r = parseInt(colorHexa.substring(1, 3), 16)
+    const g = parseInt(colorHexa.substring(3, 5), 16)
+    const b = parseInt(colorHexa.substring(5, 7), 16)
+
+    return [r, g, b]
+}
+
+// 3. Permet de dessiner la grille
 function dessin_grille(grid, data) {
     for (let pas1 = 0; pas1 < data.length; pas1++)
         for (let pas2 = 0; pas2 < data[0].length; pas2++) {
@@ -128,16 +98,17 @@ function dessin_grille(grid, data) {
         pixel.style.backgroundColor = `rgb(${data[pas1][pas2][0]}, ${data[pas1][pas2][1]}, ${data[pas1][pas2][2]})`
         pixel.id = `${pas1} ${pas2}`
         grid.appendChild(pixel) }
-  }
+}
 
-  const setBg = (cible, color, identifiant, PREFIX) => {
-
+// 4. Permet de changer la couleur des pixels de la grille
+const setBg = (cible, color, identifiant, PREFIX) => {
     let coordinates = cible.id.split(" ");
-    let y = coordinates[0].trim();  // Trim pour enlever les espaces
-    let x = coordinates[1].trim();  // Trim pour enlever les espaces
-    let r = color[0];
-    let g = color[1];
-    let b = color[2];
-    // console.log(x)
+
+    let y = coordinates[0].trim()  // Trim pour enlever les espaces
+    let x = coordinates[1].trim()
+    let r = color[0]
+    let g = color[1]
+    let b = color[2]
+
     fetch(`${PREFIX}/set/${identifiant}/${y}/${x}/${r}/${g}/${b}`, {credentials: "include"})
   }
